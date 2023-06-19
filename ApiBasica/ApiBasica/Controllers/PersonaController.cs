ea441ac73b30139e8986822f2b34e968ad8207da
CodeJuan-23 <i2226338@continental.edu.pe>
2023-06-19 13:57:26 -0500

    final api

--- ApiBasica/ApiBasica/Controllers/PersonaController.cs
+++ ApiBasica/ApiBasica/Controllers/PersonaController.cs
@@ -14,8 +14,72 @@ namespace ApiBasica.Controllers
         [HttpGet]
         public IActionResult ListarTodo()
         {
-          List<Persona> personas = db.Personas.ToList();
+            List<Persona> personas = db.Personas.ToList();
             return Ok(personas);
+        }
+
+        [HttpGet("{id}")}]
+
+        public IActionResult ObtenerPorId(int id)
+        {
+         Persona persona = db.Personas.Find(id);
+         return Ok(persona);
+        }
+
+        [HttpPost]
+
+        public IActionResult Crear([FromBody] Persona request)
+        {
+            /* 
+            db.Personas.Add(request); ==> MOTOR DE BASE DE 	  
+            DATOS, SOLO ESCRIBIMOS NUESTRO INSERT INTO
+            */
+
+        db.Personas.Add(request);
+
+        /* 
+            db.SaveChanges(); ==> SQL SERVER ==> f5 	  
+                              ==> MYSQL ==> CTRL + ENTER	 
+                             EJECUTA LA SENTENCIA ESCRITA	
+       */
+
+        db.SaveChanges();
+
+        return Ok(request);
+        }
+
+        [HttpPut]
+
+        public IActionResult Actualizar([FromBody] Persona request)
+        {
+            /* 
+            db.Personas.Add(request); ==> MOTOR DE BASE DE 	  
+            DATOS, SOLO ESCRIBIMOS NUESTRO INSERT INTO
+            */
+
+            db.Personas.Update(request);
+
+            /* 
+              db.SaveChanges(); ==> SQL SERVER ==> f5 	  
+                                ==> MYSQL ==> CTRL + ENTER	 
+                              EJECUTA LA SENTENCIA ESCRITA	
+           */
+
+            db.SaveChanges();
+
+            return Ok(request);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult EliminarPorId(int id)
+        {
+            Persona persona = db.Personas.Find(id);
+
+            db.Personas.Remove(persona);
+            db.SaveChanges();
+
+            return ok(persona);
         }
 
     }
