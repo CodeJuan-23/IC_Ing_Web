b0f834df9ee7d36c9ae6763bf717bcb09f5617ff
CodeJuan-23 <i2226338@continental.edu.pe>
2023-06-19 01:03:55 -0500

    listar-get

---
+++ ApiBasica/ApiBasica/Controllers/PersonaController.cs
@@ -0,0 +1,22 @@
+﻿using ApiBasica.Models.DBEjemplo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBasica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        /* DECLARACION DE VARIABLES GENERALES */
+        Bd01Context db = new Bd01Context();
+
+        [HttpGet]
+        public IActionResult ListarTodo()
+        {
+          List<Persona> personas = db.Personas.ToList();
+            return Ok(personas);
+        }
+
+    }
+}
